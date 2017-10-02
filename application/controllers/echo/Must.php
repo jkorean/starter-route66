b3af667550c9e1de2767481e6ae64b5efb16cc38
jkorean <jimkoexpress@hotmail.com>
2017-10-01 21:08:31 -0700

    Made Echo subcontroller in a folder closes #5
    Signed-off-by: jkorean <jimkoexpress@hotmail.com>

---
+++ application/controllers/echo/Must.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Must extends Application
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function wehave()
+    {
+        $this->show(5);
+    }
+}
